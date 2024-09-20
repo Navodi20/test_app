23cb9d2513a245b13215758351f2715c480adb3e
Navodi20 <navodidhananjana2002@gmail.com>
2024-09-20 19:14:16 +0530

    change

--- src/app/Menu/page.jsx
+++ src/app/Menu/page.jsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 
+
 const MenuPage = () => {
   const sections = [
     {
--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-content">
-          <center><p>&copy; {new Date().getFullYear()} Restaurant Name. All Rights Reserved.</p></center>
+          <center><p>&copy; {new Date().getFullYear()} My Restaurant. All Rights Reserved.</p></center>
         </div>
       </div>
     </footer>
